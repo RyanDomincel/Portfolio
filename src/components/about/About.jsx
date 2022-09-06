e1764588c852e58aa50011901d40e2d9d9ca1c58
SmileStrikeS <domincelr@gmail.com>
2022-09-06 22:27:58 +0800

    added cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import AboutImg from "../../assets/profile.jpg";
-import CV from "../../assets/Smith-Cv.pdf";
+import CV from "../../assets/RYAN CV.pdf";
 import Info from "./Info";
 
 const About = () => {
